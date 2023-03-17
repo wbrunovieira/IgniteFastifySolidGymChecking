68a85fdaa84e9d0b26b06a60dcb0c2e646871929
wbrunovieira <wbrunovieira77@gmail.com>
2023-03-17 13:40:35 -0300

    useCase eant test profile

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,18 +1,8 @@
 import { UsersRepository } from '@/repositories/users-repository'
-import { User, Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
-
-  async findByEmail(email: string) {
-    const user = this.items.find((item) => item.email === email)
-
-    if (!user) {
-      return null
-    }
-
-    return user
-  }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
@@ -24,6 +14,26 @@ export class InMemoryUsersRepository implements UsersRepository {
     }
 
     this.items.push(user)
+
+    return user
+  }
+
+  async findById(id: string) {
+    const user = this.items.find((item) => item.id === id)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
 
     return user
   }
