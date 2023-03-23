b980181e2fc45a2cac7a5f4550b7ffd6d6255c9b
wbrunovieira <wbrunovieira77@gmail.com>
2023-03-23 15:24:09 -0300

    did all factories was missing in the usecase

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -8,4 +8,5 @@ export interface GymsRepository {
   findById(id: string): Promise<Gym | null>
   create(data: Prisma.GymCreateInput): Promise<Gym>
   findManyNearby(params: FindManyNearbyParams): Promise<Gym[]>
+  searchMany(query: string, page: number): Promise<Gym[]>
 }
